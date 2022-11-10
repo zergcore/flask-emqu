7a58ea75248225c3e889d0424acc4cec75e904e5
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-10 17:00:32 -0400

    signup done

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager
-#from app.models import UserModel
 from .config import Config
 from .auth import auth
+from .models import db
+
+login_manager=LoginManager()
+login_manager.login_view='auth.login'
+
+
 
 def create_app():
     app = Flask(__name__)
     bootstrap = Bootstrap5(app)
     app.config.from_object(Config)
-    #login_manager.init_app(app)
+    login_manager.init_app(app)
     app.register_blueprint(auth)
+    db.init_app(app)
     return app
--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash
 from app.forms import LoginForm
 from . import auth
+from app.models import db, get_user, User
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -18,4 +19,19 @@ def signup():
     context={
         'signup_form':signup_form
     }
+    if signup_form.validate_on_submit():
+        email= signup_form.email.data
+        password= signup_form.password.data
+        user_row=get_user(email)
+        if user_row:
+            flash('El nombre de usuario ya existe')
+            return redirect(url_for('auth.signup'))
+        else:
+            new_user= User(email= email, password= generate_password_hash(password, method= 'sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            flash('Bienvenido(a)')
+            return redirect(url_for('hello'))
+        
     return render_template('signup.html', **context)
