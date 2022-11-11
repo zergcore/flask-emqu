7845b206f2426f6cbc96b167cf5ab45d23ea03d4
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-11 18:29:56 -0400

    models user and devices working

--- app/models.py
+++ app/models.py
@@ -3,8 +3,17 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+class Device(db.Model):
+    # __tablename__ = 'Devices'
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(45), nullable=False)
+    ipv4 = db.Column(db.String(16), nullable=False)
+    status = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('devices', lazy=True))
+
 class User(db.Model, UserMixin):
-    __tablename__= 'Users'
+    # __tablename__= 'Users'
     id= db.Column(db.Integer, primary_key= True, autoincrement= True)
     email= db.Column(db.String(30), unique= True, nullable= False)
     password= db.Column(db.String(80), nullable= False)
