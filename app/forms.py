9565045cf82d71d34e1f78dd74b8ac93466715e3
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-11 20:01:30 -0400

    Create ready, showing data ready

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email, IPAddress
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 30)], render_kw={'placeholder':'example@example.com'})
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 30)], render_kw={'placeholder':'example@example.com'})
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 20)], render_kw={'placeholder': '**************'})
-    # remember = BooleanField('Remember me')
     submit = SubmitField()
+
+class DeviceForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(),Length(4,45)])
+    ipv4 = StringField('IPv4 Address', validators=[DataRequired(), IPAddress(ipv4=True)])
+    submit = SubmitField('Create')
+
+class DeleteDeviceForm(FlaskForm):
+    submit = SubmitField('Delete')
+
+class TestDeviceForm(FlaskForm):
+    submit = SubmitField('Test')
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from re import I
 from flask import request, make_response, redirect, render_template, session, url_for, flash
 from app import create_app
-from app.models import get_users
+from app.models import get_users,get_devices, put_device
 from flask_login import login_required, logout_user
+from app.forms import DeviceForm
 
 app=create_app()
 
@@ -32,3 +33,22 @@ def dashboard():
         'user_id':user_id
     }
     return render_template('dashboard.html', **context)
+
+@app.route('/devices', methods=['GET', 'POST'])
+@login_required
+def devices():
+    user_id=session['user_id']
+    device_form=DeviceForm()
+    devices=get_devices(user_id)
+    context={
+        'user_id':user_id,
+        'devices':devices,
+        'device_form':device_form
+    }
+
+    if device_form.validate_on_submit():
+        put_device(email=user_id, name=device_form.name.data, ipv4=device_form.ipv4.data)
+        flash('Your task was created successfully!', 'success')
+        return redirect(url_for('devices'))  
+
+    return render_template('devices.html', **context)
